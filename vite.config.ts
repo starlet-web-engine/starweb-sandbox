2a0c247b893a3a9a7f12cf3667dc80ebbdbaae49
Masonlet <masonletoile@hotmail.com>
2026-06-05 14:32:35 -0400

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   publicDir: '../public',
-  base: '/web-engine-sandbox/',
+  base: '/starweb-sandbox/',
   build: {
     outDir: '../dist',
     emptyOutDir: true
